315d61cef274ba4e9e09b89155ec5199207ad82d
AlBan52 <balakin.nn@gmail.com>
2021-06-28 20:14:45 +0300

    Remove 'url' to .env
    Annother changes doesn't works

--- parse_books.py
+++ parse_books.py
@@ -1,19 +1,28 @@
 import os
 import requests
 
+from dotenv import load_dotenv
 
-url = 'https://tululu.org/txt.php'
+  
+def check_for_redirect(response):
+    if response.status_code != 200:
+       raise HTTPError
+    else:
+       pass
 
-os.makedirs('books', exist_ok=True)
+        
+if __name__ == '__main__':
+    load_dotenv()
+    url = os.getenv('URL_FOR_DOWNLOADING')
+    os.makedirs('books', exist_ok=True)
 
-book_id = 1
-books_number = 10
+    book_id = 1
+    books_number = 10
 
-while book_id <= books_number:
-    response = requests.get(url, params={'id': book_id})
-    response.raise_for_status()
+    while book_id <= books_number:
+        response = requests.get(url, params={'id': book_id}, allow_redirects=False)
+        response.raise_for_status()
 
-    with open(f'books/{book_id}.txt', 'w', encoding='utf-8') as file:
-        file.write(response.text)
+        with open(f'books/{book_id}.txt', 'w', encoding='utf-8') as file:
+            file.write(response.text)
     book_id += 1
-    
