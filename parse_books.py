fe00ef7b2eff8e0f5dccb113530128f7e566efb4
AlBan52 <balakin.nn@gmail.com>
2021-07-02 20:36:26 +0300

    Fix bugs, redirect cases is workable

--- parse_books.py
+++ parse_books.py
@@ -16,13 +16,16 @@ if __name__ == '__main__':
     url = os.getenv('URL_FOR_DOWNLOADING')
     os.makedirs('books', exist_ok=True)
 
-    book_id = 1
     books_number = 10
 
-    while book_id <= books_number:
+    for book_id in range(1, books_number+1):
         response = requests.get(url, params={'id': book_id}, allow_redirects=False)
         response.raise_for_status()
+        try:
+            check_for_redirect(response)
+        except:
+            continue
 
         with open(f'books/{book_id}.txt', 'w', encoding='utf-8') as file:
             file.write(response.text)
-    book_id += 1
+        
