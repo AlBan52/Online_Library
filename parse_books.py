e9619716b04168f8ef952edf4f733099a3d5e6a0
AlBan52 <balakin.nn@gmail.com>
2021-08-03 20:01:25 +0300

    download books with titles
    Commit is not atomic:
     - add 4 new functions (get_book_title(), create_filename(), create_filepath(), download_txt()
    - some bugs fixed

--- parse_books.py
+++ parse_books.py
@@ -3,9 +3,10 @@ import requests
 
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from pathvalidate import sanitize_filename
 
   
-def get_response(url, header):
+def get_response(url, header=None):
     response = requests.get(url, params=header, allow_redirects=False)
     response.raise_for_status()
 
@@ -17,13 +18,39 @@ def check_for_redirect(response):
     else:
        pass
 
-def download_txt(url, filename, folder='books'):
-    response = get_response(url, header)
-    filename = f'{sanitize_filename(filename)}.txt'
+def get_book_title(url, book_id):
+    url = f'{url}b{book_id}/'
+    response = get_response(url)
+    try:
+        check_for_redirect(response)
+    except:
+        return
+    soup = BeautifulSoup(response.text, 'lxml')
+    book_name_and_author = soup.find(id = 'content').find('h1')
+    book_title = book_name_and_author.get_text().split('::')[0].strip()
+
+    return book_title
+
+def create_filename(book_title, book_id):
+    filename = f'{sanitize_filename(book_title)}.txt'
+    filename = f'{book_id}_{filename}'
+
+    return filename
+
+def create_filepath(filename, folder='books'):
     filepath = os.path.join(folder, filename)
-    with open(filepath, 'w', encoding='utf-8') as file: file.write(response.text)
 
     return filepath
+
+def download_txt(url, filepath):
+    url = f'{url}txt.php'
+    response = get_response(url, header={'id': book_id})
+    try:
+        check_for_redirect(response)
+    except:
+        return
+    with open(filepath, 'w', encoding='utf-8') as file: file.write(response.text)
+
         
 if __name__ == '__main__':
     load_dotenv()
@@ -33,14 +60,12 @@ if __name__ == '__main__':
     books_number = 10
 
     for book_id in range(1, books_number+1):
-        header = {'id': book_id}
-        response = get_response(url, header)
-        
-        try:
-            check_for_redirect(response)
-        except:
+        book_title = get_book_title(url, book_id)
+        if book_title:
+            filename = create_filename(book_title, book_id)
+            filepath = create_filepath(filename, folder='books')
+        else:
             continue
-
-        with open(f'books/{book_id}.txt', 'w', encoding='utf-8') as file:
-            file.write(response.text)
+        
+        download_txt(url, filepath)   
         
